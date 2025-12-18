94ec671dc3bba315fa023a2e9ee19fb8415b2181
Luc Hermitte <luc.hermitte@cs-soprasteria.com>
2025-12-18 11:55:11 +0100

    ENH: Fix (re)throw statements in otbMacro.h

--- Modules/Core/Common/include/otbMacro.h
+++ Modules/Core/Common/include/otbMacro.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2024 Centre National d'Etudes Spatiales (CNES)
+ * Copyright (C) 2005-2025 Centre National d'Etudes Spatiales (CNES)
  *
  * This file is part of Orfeo Toolbox
  *
@@ -9,7 +9,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -202,9 +202,9 @@ namespace otb
     {                                                                                       \
       command;                                                                              \
     }                                                                                       \
-    catch (std::bad_alloc & err)                                                            \
+    catch (std::bad_alloc const& err)                                                       \
     {                                                                                       \
-      throw err;                                                                            \
+      throw;                                                                                \
     }                                                                                       \
     catch (itk::ExceptionObject&)                                                           \
     {                                                                                       \
@@ -213,7 +213,7 @@ namespace otb
     }                                                                                       \
     catch (const std::exception& stde)                                                      \
     {                                                                                       \
-      throw stde;                                                                           \
+      throw;                                                                                \
     }                                                                                       \
     catch (...)                                                                             \
     {                                                                                       \
