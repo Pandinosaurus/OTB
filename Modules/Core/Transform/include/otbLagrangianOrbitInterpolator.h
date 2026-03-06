717f635bd2b017ea98ce0f1c8c7a9d8d9ad47913
Luc Hermitte <luc.hermitte@cs-soprasteria.com>
2026-03-06 18:58:12 +0100

    STY: Fix indent

--- Modules/Core/Transform/include/otbLagrangianOrbitInterpolator.h
+++ Modules/Core/Transform/include/otbLagrangianOrbitInterpolator.h
@@ -52,64 +52,64 @@ public:
   explicit LagrangianOrbitInterpolator(
       std::vector<Orbit> const& orbits,
       unsigned int polynomial_degree = 8)
-     : m_polynomial_degree(polynomial_degree - 2)
-     , m_orbits(orbits)
-     {
-       // Legacy implementation used to misinterpret "degree".
-       // For a polynom of degree 8, 8 values shall be used:
-       // \Sum_{i=0}^{n} \Mult_{j=0, j!=i=^{n} \frac{X - x_i}{x_j - x-i}
-       // => 9 values for i ([0..n]), 8 values for j ([0..i[ U ]i..n])
-       //
-       // Let's assume a center at idx=7, and degree 8
-       // legacyBegin = idx-deg/2+1 = 4
-       // legacyEnd   = legacyBegin + deg - 1 = 4+8-1 = 11
-       // and we loop in [legacyBegin..legacyEnd[ => 7 values
-       // IOW, real degree is 6 (7-1)!
+  : m_polynomial_degree(polynomial_degree - 2)
+  , m_orbits(orbits)
+  {
+    // Legacy implementation used to misinterpret "degree".
+    // For a polynom of degree 8, 8 values shall be used:
+    // \Sum_{i=0}^{n} \Mult_{j=0, j!=i=^{n} \frac{X - x_i}{x_j - x-i}
+    // => 9 values for i ([0..n]), 8 values for j ([0..i[ U ]i..n])
+    //
+    // Let's assume a center at idx=7, and degree 8
+    // legacyBegin = idx-deg/2+1 = 4
+    // legacyEnd   = legacyBegin + deg - 1 = 4+8-1 = 11
+    // and we loop in [legacyBegin..legacyEnd[ => 7 values
+    // IOW, real degree is 6 (7-1)!
     assert(m_polynomial_degree < 30); // Lagrangian interpolator fails miserably at 20 elements...
                                       // Let's expected less than 30 as reasonable
 
     m_buffer.resize(2 * m_polynomial_degree * orbits.size());
-       m_inv_den = inv_den_view_t(
-           m_buffer.data(), narrow{},
-           orbits.size(),
-           2 * m_polynomial_degree);
+    m_inv_den = inv_den_view_t(
+        m_buffer.data(), narrow{},
+        orbits.size(),
+        2 * m_polynomial_degree);
 
-       auto nLast = m_orbits.size();
+    auto nLast = m_orbits.size();
     for (std::size_t nBegin = 0; nBegin < nLast; ++nBegin)
-       {
+    {
       auto const nEnd       = std::min(nLast, nBegin + m_polynomial_degree + 1);
-         // ∀ i ∈ [nBegin..nEnd[
-         // --> inv_den(nBegin, i) = TT_{j!=i} t[i] - t[j]
+      // ∀ i ∈ [nBegin..nEnd[
+      // --> inv_den(nBegin, i) = TT_{j!=i} t[i] - t[j]
       auto const offset_max = std::min(nLast - nBegin, std::size_t(1 + m_polynomial_degree));
       for (std::size_t offset_i = 0; offset_i < offset_max; ++offset_i)
-         {
-           std::ptrdiff_t i = offset_i + nBegin;
-           assert(i < std::ptrdiff_t(nLast));
-           auto const t_i = m_orbits[i].time;
+      {
+        std::ptrdiff_t i = offset_i + nBegin;
+        assert(i < std::ptrdiff_t(nLast));
+        auto const t_i = m_orbits[i].time;
 
-           double w = 1.;
-           unsigned int j = nBegin;
+        double w = 1.;
+        unsigned int j = nBegin;
         for (; j < i; ++j)
-           {
-             assert(i != j);
-             auto const den = t_i - m_orbits[j].time;
-             assert(den.NumberOfTicks() != 0);
+        {
+          assert(i != j);
+          auto const den = t_i - m_orbits[j].time;
+          assert(den.NumberOfTicks() != 0);
           w *= den.NumberOfTicks();
-           }
+        }
         for (++j; j < nEnd; ++j)
-           {
-             assert(i != j);
-             auto const den = t_i - m_orbits[j].time;
-             assert(den.NumberOfTicks() != 0);
+        {
+          assert(i != j);
+          auto const den = t_i - m_orbits[j].time;
+          assert(den.NumberOfTicks() != 0);
           w *= den.NumberOfTicks();
-           }
-           assert(nBegin < m_orbits.size());
-           assert(offset_i < 1 + m_polynomial_degree);
-           assert(w != 0);
-           m_inv_den(unsigned(nBegin), unsigned(offset_i)) = 1.0 / w;
-         }
-       }
-     }
+        }
+        assert(nBegin < m_orbits.size());
+        assert(offset_i < 1 + m_polynomial_degree);
+        assert(w != 0);
+        m_inv_den(unsigned(nBegin), unsigned(offset_i)) = 1.0 / w;
+      }
+    }
+  }
 
   std::pair<Orbit::PointType, Orbit::PointType> interpolatePosVel(
       TimePoint time, std::size_t nBegin, std::size_t nEnd) const
