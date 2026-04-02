21d73dd9a50d94fbcc6390f894fdd40e90c65283
Tristan Laurent <tristan.laurent@cs-soprasteria.com>
2026-04-02 11:26:50 +0200

    CI: correct legal-check job

--- CMake/OTBOptions.cmake
+++ CMake/OTBOptions.cmake
@@ -1,4 +1,23 @@
-# NOTE TLA : to move later to upper level.
+#
+# Copyright (C) 2005-2026 Centre National d'Etudes Spatiales (CNES)
+#
+# This file is part of Orfeo Toolbox
+#
+#     https://www.orfeo-toolbox.org/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Old behavior was controled by otb_module_activation_option in otb-module files
 option(OTB_USE_6S "Enable 6S radiometric and atmospheric correction module" ON)
 option(OTB_USE_GSL "Enable GSL dependent modules" OFF)
