a83d77b67838a3435a19772e0c492ddb7068759e
Tristan Laurent <tristan.laurent@cs-soprasteria.com>
2026-07-30 15:17:21 +0200

    CI: use Visual Studio code tools v14.44 to compile in cpp20

---
+++ CI/windows-10-x64-vc14.51.cmake
@@ -0,0 +1,68 @@
+#
+# Copyright (C) 2005-2026 Centre National d'Etudes Spatiales (CNES)
+#
+# This file is part of Orfeo Toolbox
+#
+#     https://www.orfeo-toolbox.org/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Configuration options for windows-10-x64-vc14.51.cmake
+# this build tool can compile C++20 project
+
+set(site_option
+    "CMAKE_CXX_FLAGS:STRING=/DTHROW_QCRITICAL=0 /DWIN32 /D_WINDOWS /W3 /GR /EHsc /D_ENABLE_EXTENDED_ALIGNED_STORAGE"
+    "OTB_USE_OPENMP:BOOL=ON"
+)
+
+if (OTB_FULL_BUILD)
+  list(APPEND site_option
+    "OTB_BUILD_FeaturesExtraction:BOOL=ON"
+    "OTB_BUILD_Hyperspectral:BOOL=ON"
+    "OTB_BUILD_Learning:BOOL=ON"
+    "OTB_BUILD_Miscellaneous:BOOL=ON"
+    "OTB_BUILD_Remote:BOOL=ON"
+    "OTB_BUILD_SAR:BOOL=ON"
+    "OTB_BUILD_Segmentation:BOOL=ON"
+    "OTB_BUILD_StereoProcessing:BOOL=ON"
+    "OTB_USE_LIBSVM:BOOL=ON"
+    "OTB_USE_MUPARSER:BOOL=ON"
+    "OTB_USE_MUPARSERX:BOOL=ON"
+    "OTB_USE_OPENCV:BOOL=ON"
+    "OTB_USE_SHARK:BOOL=ON")
+else()
+  list(APPEND site_option
+    "OTB_BUILD_FeaturesExtraction:BOOL=OFF"
+    "OTB_BUILD_Hyperspectral:BOOL=OFF"
+    "OTB_BUILD_Learning:BOOL=OFF"
+    "OTB_BUILD_Miscellaneous:BOOL=OFF"
+    "OTB_BUILD_Remote:BOOL=OFF"
+    "OTB_BUILD_SAR:BOOL=OFF"
+    "OTB_BUILD_Segmentation:BOOL=OFF"
+    "OTB_BUILD_StereoProcessing:BOOL=OFF"
+    "OTB_USE_LIBSVM:BOOL=OFF"
+    "OTB_USE_MUPARSER:BOOL=OFF"
+    "OTB_USE_MUPARSERX:BOOL=OFF"
+    "OTB_USE_OPENCV:BOOL=OFF"
+    "OTB_USE_SHARK:BOOL=OFF")
+endif()
+
+# As windows have a dumb dynamic linkage system, we need to use
+# the XDK_INSTALL_PATH option to ensure dlls are copied from xdk to otb bin
+# folder 
+if(XDK_INSTALL_PATH)
+  set(site_option
+      "XDK_INSTALL_PATH=${XDK_INSTALL_PATH}"
+      "${site_option}")
+endif()
